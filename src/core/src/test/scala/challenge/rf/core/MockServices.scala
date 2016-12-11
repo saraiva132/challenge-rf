12e837b338e9b7debb5d4fceebd616af43994ad8
Rafael <rafael.saraiva@ua.pt>
2016-12-11 15:57:16 -0100

    Refactor mock services sleep time place. Causing print expectation errors.

--- src/core/src/test/scala/challenge/rf/core/MockServices.scala
+++ src/core/src/test/scala/challenge/rf/core/MockServices.scala
@@ -4,14 +4,14 @@ import challenge.rf.api.{OK, Result, Service}
 
 class ServiceExample1 extends Service {
   override def start(): Result = {
-    println("Starting ServiceExample1")
     Thread.sleep(200)
+    println("Starting ServiceExample1")
     OK
   }
 
   override def stop(): Result = {
-    println("Stopping ServiceExample1")
     Thread.sleep(200)
+    println("Stopping ServiceExample1")
     OK
   }
 
@@ -22,14 +22,14 @@ class ServiceExample1 extends Service {
 
 class ServiceExample2 extends Service {
   override def start(): Result = {
-    println("Starting ServiceExample2")
     Thread.sleep(200)
+    println("Starting ServiceExample2")
     OK
   }
 
   override def stop(): Result = {
-    println("Stopping ServiceExample2")
     Thread.sleep(200)
+    println("Stopping ServiceExample2")
     OK
   }
 
@@ -40,14 +40,14 @@ class ServiceExample2 extends Service {
 
 class ServiceExample3 extends Service {
   override def start(): Result = {
-    println("Starting ServiceExample3")
     Thread.sleep(200)
+    println("Starting ServiceExample3")
     OK
   }
 
   override def stop(): Result = {
-    println("Stopping ServiceExample3")
     Thread.sleep(200)
+    println("Stopping ServiceExample3")
     OK
   }
 
@@ -58,14 +58,14 @@ class ServiceExample3 extends Service {
 
 class ServiceExample4 extends Service {
   override def start(): Result = {
-    println("Starting ServiceExample4")
     Thread.sleep(200)
+    println("Starting ServiceExample4")
     OK
   }
 
   override def stop(): Result = {
-    println("Stopping ServiceExample4")
     Thread.sleep(200)
+    println("Stopping ServiceExample4")
     OK
   }
 
